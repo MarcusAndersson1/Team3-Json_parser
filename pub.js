a2129115a4fa6ba9fc2488b575474c478ca48a5c
Ed <edvin.33d@gmail.com>
2022-01-06 15:58:26 +0100

    work in progress MQTT

--- pub.js
+++ pub.js
@@ -11,76 +11,85 @@ const clinic = {
     "address": "Slottskogen",
     "city": "Gothenburg",
     "coordinate": {
-      "longitude": 11.942625,
-      "latitude": 57.685255
+        "longitude": 11.942625,
+        "latitude": 57.685255
     },
     "openinghours": {
-      "monday": "7:00-19:00",
-      "tuesday": "7:00-19:00",
-      "wednesday": "7:00-19:00",
-      "thursday": "7:00-19:00",
-      "friday": "7:00-19:00",
-      "sunday": ""
+        "monday": "7:00-19:00",
+        "tuesday": "7:00-19:00",
+        "wednesday": "7:00-19:00",
+        "thursday": "7:00-19:00",
+        "friday": "7:00-19:00",
+        "sunday": ""
     }
-  }
-
-
- 
-
-var pubTimeslot = function sendTimeslot(timeslot){
-    client.on('connect', function(){
-            if(timeslot===undefined){
-                client.end()
-           
-            }else{
-                client.publish('Timeslot',timeslot)
-            }
-    })
-}
-
-function sendTimeslot(timeslot){
-    client.on('connect', function(){
-        
-            if(timeslot===undefined){
-                client.end()
-                
-            }else{
-                client.publish('Timeslot',timeslot)
-                console.log(timeslot)
-            }
-    })
-}
-
-var pubClinic = function sendClinic(clinic){
-    client.on('connect', function(){
-            if(clinic===undefined){
-                client.end()
-            }else{
-                client.publish('Clinic', JSON.stringify(clinic))
-                console.log(JSON.stringify(clinic))
-            }
-            
-    })
-
 }
 
 
-function sendClinic(clinic){
-    client.on('connect', function(){
-            if(clinic===undefined){
-                client.end()
-            }else{
-                client.publish('Clinic', JSON.stringify(clinic))
-                console.log(JSON.stringify(clinic))
-            }
-            
+
+
+var pubTimeslot = function sendTimeslot(timeslot) {
+    client.on('connect', function () {
+        if (timeslot === undefined) {
+            client.end()
+
+        } else {
+            client.publish('Timeslot', timeslot)
+            // console.log("Publishing" + timeslot)
+        }
+    })
+}
+client.subscribe("Clinic/true")
+
+var listen = function listen() {
+    client.on('message', function (topic, message) {
+        console.log(message + topic)
+    }
+    )
+}
+
+function sendTimeslot(timeslot) {
+    client.on('connect', function () {
+
+        if (timeslot === undefined) {
+            client.end()
+
+        } else {
+            client.publish('Timeslot', timeslot)
+            console.log(timeslot)
+        }
+    })
+}
+
+
+var pubClinic = function sendClinic(clinic) {
+    client.on('connect', function () {
+        if (clinic === undefined) {
+            client.end()
+        } else {
+            client.publish('Clinic', JSON.stringify(clinic))
+            console.log(JSON.stringify(clinic))
+        }
+
+    })
+
+}
+
+
+function sendClinic(clinic) {
+    client.on('connect', function () {
+        if (clinic === undefined) {
+            client.end()
+        } else {
+            client.publish('Clinic', JSON.stringify(clinic))
+            console.log(JSON.stringify(clinic))
+        }
+
     })
 
 }
 
 //sendClinic(clinic)
 //sendTimeslot("2023")
-    
 
 
 
@@ -89,4 +98,4 @@ function sendClinic(clinic){
 
 
 
-module.exports = {pubTimeslot, pubClinic};
+module.exports = { pubTimeslot, pubClinic, listen };
--- timeSlotParser.ts
+++ timeSlotParser.ts
@@ -1,49 +1,66 @@
 var publish = require("./pub")
 
+var date = new Date();
+var json = JSON.stringify(date);
+
 const dentists = require('./dentists.json');
 
 var arr = dentists['dentists']
 
-const weekdays = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
+const weekdays = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+async function timeSlotParser() {
+  for (let index = 0; index < arr.length; index++) { // Specific Clinic
+    console.log("\nDentist Clinic: " + arr[index]['name'])
 
-for (let index = 0; index < arr.length; index++) { // Specific Clinic
-  console.log("\nDentist Clinic: " + arr[index]['name'])
+    console.log(arr[index])
+    await publish.pubClinic(arr[index])
+    let counter = 0
+    let arrOpeningHours = arr[index]['openinghours']
+    // let daysOpen = Object.keys(arrOpeningHours)
+    // let availableTimes = [] // Creates array of available times
 
+    // INSERT CODE THAT WAITS FOR WHEN RESPONSE FOR pubClinic IS RECIEVED
 
-  let arrOpeningHours = arr[index]['openinghours']
-  // let daysOpen = Object.keys(arrOpeningHours)
-  arr[index]['availableTimes'] = [] // Creates array of available times
+    for (let i = 0; i <= 6; i++) { // Clinic's specific day
+      var d = new Date();
+      d.setDate(d.getDate() + i)
+      let day = weekdays[d.getDay()]
 
-  for (let i = 0; i <= 1; i++){ // Clinic's specific day
-    var d = new Date();
-    d.setDate(d.getDate() + i)
-    let day = weekdays[d.getDay()]
+      let openingTime = arrOpeningHours[day]
+      if (arrOpeningHours[day] === undefined || openingTime === "") {
+        console.log("SKIPPING, Day was " + day + " and hours were " + openingTime)
+        continue;
+      }
 
-    publish(d.toString()) // SUBJECT TO CHANGE, we are not using hours yet
-
-    let openingTime = arrOpeningHours[day]
-    if (arrOpeningHours[day] === undefined || openingTime === "") {
-      console.log("SKIPPING, Day was " + day + " and hours were " + openingTime)
-      continue;
-    }
-
-    let startingHour = openingTime.match(/^(\d+)/)[0] // the 0 is to only grab the regex result and not additional info as it creates an array
-    let endingHour = openingTime.match(/.(\d{2}):\d{2}$/)[1] // regex grabs the digits from the ending hour, e.g. 9:00-[ 17 ]:00
-    startingHour = parseInt(startingHour)
-    endingHour = parseInt(endingHour)
-    console.log(day + " opening hours: " + openingTime)
-    for (let minutes = 0; minutes < 1440; minutes = (minutes + 30)) { // Specific hour of day
-        d.setHours(startingHour,minutes,0)
-        if(d.getHours() === 12 && endingHour != 12) { // Lunchbreak hour
+      let startingHour = openingTime.match(/^(\d+)/)[0] // the 0 is to only grab the regex result and not additional info as it creates an array
+      let endingHour = openingTime.match(/.(\d{2}):\d{2}$/)[1] // regex grabs the digits from the ending hour, e.g. 9:00-[ 17 ]:00
+      startingHour = parseInt(startingHour)
+      endingHour = parseInt(endingHour)
+      console.log(day + " opening hours: " + openingTime)
+      for (let minutes = 0; minutes < 1440; minutes = (minutes + 30)) { // Specific hour of day
+        d.setHours(startingHour, minutes, 0)
+        if (d.getHours() === 12 && endingHour != 12) { // Lunchbreak hour
           continue
         }
-        arr[index]['availableTimes'].push(new Date(d))
+        console.log(new Date(d).toString())
 
-        if(d.getHours() === endingHour) {
+        publish.pubTimeslot(new Date(d).toString())
+
+        counter++
+
+        if (d.getHours() === endingHour) {
           break
         }
+      }
     }
+    console.log("Amount of timeslots: " + counter)
   }
+  console.log("-- End of Parsing --")
+  console.log((arr[0]))
 }
-console.log("-- End of Parsing --")
-// console.log(arr[0]['availableTimes'])
+
+timeSlotParser()
+// > CLINIC 1
+// > 150 timeslots
+// > CLINIC 2
+// > 90 timeslots
